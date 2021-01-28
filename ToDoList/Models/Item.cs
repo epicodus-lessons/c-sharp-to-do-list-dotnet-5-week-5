01e2cc45aab84470f76e641261ef1c2f0d855fdb
Travis Scott <travisty12@gmail.com>
2021-01-28 11:28:11 -0800

    initial commit

---
+++ ToDoList/Models/Item.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+
+namespace ToDoList.Models
+{
+  public class Item
+  {
+    public Item()
+    {
+      this.JoinEntities = new HashSet<CategoryItem>();
+    }
+
+    public int ItemId { get; set; }
+    public string Description { get; set; }
+
+    public virtual ICollection<CategoryItem> JoinEntities { get;}
+  }
+}
