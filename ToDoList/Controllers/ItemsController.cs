9bee5097befaa115ccdc400160b5022750a765fe
Travis Scott <travisty12@gmail.com>
2021-03-11 14:06:19 -0800

    update lambdas for best practices

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -51,7 +51,7 @@ namespace ToDoList.Controllers
 
     public ActionResult Edit(int id)
     {
-      var thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
+      var thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
       ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
       return View(thisItem);
     }
@@ -70,7 +70,7 @@ namespace ToDoList.Controllers
 
     public ActionResult AddCategory(int id)
     {
-      var thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
+      var thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
       ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
       return View(thisItem);
     }
@@ -88,14 +88,14 @@ namespace ToDoList.Controllers
 
     public ActionResult Delete(int id)
     {
-      var thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
+      var thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
       return View(thisItem);
     }
 
     [HttpPost, ActionName("Delete")]
     public ActionResult DeleteConfirmed(int id)
     {
-      var thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
+      var thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
       _db.Items.Remove(thisItem);
       _db.SaveChanges();
       return RedirectToAction("Index");
