bbae62a5db7e159a2c64cde330bd1a92db152faf
Brooke Kullberg <48640716+BrookeZK@users.noreply.github.com>
2021-07-07 13:20:54 -0700

    remove unnecessary code

--- ToDoList/Models/ToDoListContext.cs
+++ ToDoList/Models/ToDoListContext.cs
@@ -5,7 +5,7 @@ namespace ToDoList.Models
 {
   public class ToDoListContext : IdentityDbContext<ApplicationUser>
   {
-    public virtual DbSet<Category> Categories { get; set; }
+    public DbSet<Category> Categories { get; set; }
     public DbSet<Item> Items { get; set; }
     public DbSet<CategoryItem> CategoryItem { get; set; }
 
